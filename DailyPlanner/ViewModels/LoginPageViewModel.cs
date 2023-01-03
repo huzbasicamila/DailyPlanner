3979697840e670f47177cc505e27cfefc6e572f0
Amila Huzbasic <92604250+huzbasicamila@users.noreply.github.com>
2023-01-04 00:06:59 +0100

    Login

--- DailyPlanner/ViewModels/LoginPageViewModel.cs
+++ DailyPlanner/ViewModels/LoginPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace DailyPlanner.ViewModels
 {
-    public partial class LoginPageViewModel
+    public partial class LoginPageViewModel:BaseViewModel
     {
+        [ObservableProperty]
+        private string _userName;
+        [ObservableProperty]
+         private string _password;
     }
 }
---
+++ DailyPlanner/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DailyPlanner.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
